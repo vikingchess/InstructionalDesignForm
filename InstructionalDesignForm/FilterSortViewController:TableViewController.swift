1b951483e6a334e635d29168b134124a75abeb3a
David Flom <davidflom01@gmail.com>
2018-05-09 15:52:00 -0400

    finished filter view controller project builds

--- InstructionalDesignForm/FilterSortViewController:TableViewController.swift
+++ InstructionalDesignForm/FilterSortViewController:TableViewController.swift
@@ -10,88 +10,38 @@ import UIKit
 import CoreData
 
 class FilterSortViewController_TableViewController: UITableViewController {
+    // MARK: - Properties
         var coreDataStack: CoreDataStack!
-
+    lazy var totalNumberOfProjects: NSPredicate = {
+        return NSPredicate(format: "%K LIKE[c] %@", #keyPath(Data.name),"*")
+    }()
+    
+    
+    //MARK: - Outlets
+    
+    @IBOutlet weak var numberOfProjects: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        populateNumberOfProjects()
 
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+//MARK: - Helper methods
+extension FilterSortViewController_TableViewController {
+    func populateNumberOfProjects(){
+        let fetchRequest = NSFetchRequest<NSNumber>(entityName: "Data")
+        fetchRequest.resultType = .countResultType
+        fetchRequest.predicate = totalNumberOfProjects
+        do {
+            let countResult = try coreDataStack.managedContext.fetch(fetchRequest)
+            let count = countResult.first!.intValue
+            numberOfProjects.text = "\(count) Total Projects"
+        } catch let error as NSError {
+            print("could not fetch \(error)")
+        }
+    }
+}
+
+
+
